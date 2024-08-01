d40dd17240731a47a20a521654a8dfd0392933ba
gaurimadan <gaurimadan12@gmail.com>
2024-08-02 00:37:46 +0530

    main template added

--- my_portfolio/app/page.tsx
+++ my_portfolio/app/page.tsx
@@ -1,13 +1,23 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import Grid from "@/components/Grid";
+// import { navItems } from "@/data";
+
+import Hero from "@/components/Hero";
+
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+
+const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        
+        {/* <FloatingNav navItems={navItems} /> */}
+        <Hero />
+        <Grid/>
+      
       </div>
-   </main>
-     
-   
+    </main>
   );
 };
+
+export default Home;
