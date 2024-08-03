c3dbcf0b9a83f315f8b466b187d9cd6839f101fc
gaurimadan <gaurimadan12@gmail.com>
2024-08-03 18:58:22 +0530

    projects completed

--- my_portfolio/app/page.tsx
+++ my_portfolio/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Grid from "@/components/Grid";
-// import { navItems } from "@/data";
+import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -12,7 +12,7 @@ const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        {/* <FloatingNav navItems={navItems} /> */}
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid/>
         <RecentProjects/>
