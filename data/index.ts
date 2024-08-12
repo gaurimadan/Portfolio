e8c855a6ef196cc481803e60f790d68bcb5b9462
gaurimadan <gaurimadan12@gmail.com>
2024-08-12 16:16:21 +0530

    init

---
+++ data/index.ts
@@ -0,0 +1,146 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Work Experience", link: "#workexperience" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "My Goal is to convert concepts into tangible, interactive experience",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "Download my resume here!",
+      description: "Want to know more about my journey?",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "Designing Tomorrow's Web with Today's Technologies",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-3",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently working with a startup and helping them bring their vission to reality",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Book Recommendation System",
+      des: "Developed and trained a ML model that utilizes KNN algorithm to recommend books.It is a collaborative recommendation system.",
+      img: "/p1.svg",
+      iconLists: ["/icons8-python.svg"],
+      link: "https://github.com/adrianhajdin?tab=repositories",
+    },
+    {
+      id: 2,
+      title: "MoneyMaven- A finance tracking App",
+      des: "Designed an inclusive FinTech solution that empowers them to manage their money effectively and build a secure financial foundation.Investment recommendations and retirement planning.",
+      img: "/Desktop - 4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "https://github.com/adrianhajdin/zoom-clone",
+    },
+    {
+      id: 3,
+      title: "ZestAI - Text,Image, Video, Code, Music Generation App",
+      des: "Developed an AI SaaS application capable of generating photo, video, text, music, and code.Designed and managed databases using Prisma with MongoDB to track API usage and manage user data.Implemented mechanisms to track and limit API usage to prevent abuse and ensure fair usage.Created a user-friendly and responsive interface using shadcn UI",
+      img: "/Screenshot from 2024-08-08 04-02-34.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/c.svg"],
+      link: "https://github.com/adrianhajdin/ai_saas_app",
+    },
+    {
+      id: 4,
+      title: "Industrial Management System",
+      des: " Developed an EHS solution for Indian industry that ensures healthy working conditions in industries.",
+      img: "/p4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/adrianhajdin/iphone",
+    },
+  ];
+  
+
+  
+  
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Engineer Intern",
+      desc: "Building reusable and maintainable components with Material-UI to ensure consistent design and functionality across the application.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "ROS Intern",
+      desc: "Designed, animation and simulation of drones using softwares like Blender, Fusion360 and Gazebo",
+      className: "md:col-span-2",
+      thumbnail: "/exp2.svg",
+    },
+  
+    
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      url:"https://github.com/gaurimadan"
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+      url:"https://x.com/gaurimadan"
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      url:"https://www.instagram.com/_gaurimadaan__/"
+    },
+  ];
